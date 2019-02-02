ba0778a9cd1b16c73453741da74fbb20b4663dfa
Manu <28782438+manu-p-1@users.noreply.github.com>
2019-02-01 20:36:58 -0500

    Abstracted directory structure

--- Utilities/Drivers/Driver.cs
+++ Utilities/Drivers/Driver.cs
@@ -5,7 +5,7 @@ public class Driver
     public static void Main(string[] args)
     {
         Console.WriteLine(StringUtils.Remove("Manu", 'r'));
-        Console.WriteLine(StringUtils.isPalindromeIgnoreCase("malayalam"));
+        Console.WriteLine(StringUtils.IsPalindromeIgnoreCase("malayalam"));
         Console.Read();
     }
 }

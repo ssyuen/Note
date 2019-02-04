2ae8fc8ff3ec6728fc23fdede4df8ef2818479f4
ssyuen <samuel.s.yuen@gmail.com>
2019-02-03 23:41:52 -0500

    i think i fixed the error handling by adding try catch?

--- Utilities/ArrayUtils/ArrayUtils.cs
+++ Utilities/ArrayUtils/ArrayUtils.cs
@@ -10,10 +10,8 @@ using System.Linq.Expressions;
 
 namespace Utilities
 {
-
     public class ArrayUtils
     {
-
         /// <summary>
         /// Concatenates two arrays of the same type together.
         /// </summary>
@@ -21,29 +19,25 @@ namespace Utilities
         /// <param name="array2">The string to be concatenated</param>
         /// <returns>The concatenated array</returns>
         public static T[] Concat<T>(T[] array1, T[] array2)
-        { 
+        {
             T[] array3 = new T[array1.Length + array2.Length];
 
             for (int i = 0; i < array1.Length; i++)
             {
                 array3[i] = array1[i];
             }
+
             for (int i = 0; i < array2.Length; i++)
             {
                 array3[array1.Length + i] = array2[i];
             }
-            return array3;
 
-        }//array concat
+            return array3;
+        } //array concat
 
         public static double FindAvg<T>(T[] array1)
         {
-            if (array1.Equals(typeof(String)) || array1.Equals(typeof(object)))
-            {
-                throw new ArrayAccomodatingNonRealNumberException();
-                return 0;
-            }
-            else
+            try
             {
                 double sum = 0;
                 int denom = array1.Length;
@@ -51,11 +45,18 @@ namespace Utilities
 
                 for (int i = 0; i < array1.Length; i++)
                 {
-                    sum += (dynamic)array1[i];
+                    sum += (dynamic) array1[i];
                 }
-
                 return avg = sum / denom;
             }
-        }//findavg
-    }//arrayutils
+            catch
+            {
+                throw new ArrayAccomodatingNonRealNumberException();
+                return 0;
+            }
+
+            return 0;
+
+        } //findavg
+    } //arrayutils
 }
